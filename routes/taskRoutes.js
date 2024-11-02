cc177c8926c8538445e69202f7b363d08423e124
Anisa-hussain <anisoyarey280@gmail.com>
2024-11-02 22:03:30 +0300

    TaskRoutes

---
+++ routes/taskRoutes.js
@@ -0,0 +1,20 @@
+
+// routes/taskRoutes.js
+const { getTasks, createTask, updateTask, deleteTask } = require('../controllers/taskController');
+
+const taskRoutes = (req, res) => {
+    if (req.method === 'GET') {
+        getTasks(req, res);
+    } else if (req.method === 'POST') {
+        createTask(req, res);
+    } else if (req.method === 'PATCH') {
+        updateTask(req, res);
+    } else if (req.method === 'DELETE') {
+        deleteTask(req, res);
+    } else {
+        res.writeHead(405, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ message: 'Method Not Allowed.' }));
+    }
+};
+
+module.exports = taskRoutes;
